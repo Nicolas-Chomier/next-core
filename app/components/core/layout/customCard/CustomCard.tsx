42679ee6db9078b03de8a9f0e4d099d7e53afa28
Nicolas Chomier <nicolas.chomier@protonmail.com>
2023-11-26 11:20:37 +0100

    New style for custom card

--- app/components/core/layout/customCard/CustomCard.tsx
+++ app/components/core/layout/customCard/CustomCard.tsx
@@ -1,22 +1,26 @@
 // React core
 import React from 'react';
 // External modules / Third-party libraries
-import { Card } from '@radix-ui/themes';
 // Local components
 // Hooks and utilities
+import { setDarkMode } from '@/app/store/core/darkMode';
 // Configuration
 import { NATIVE_COMPONENT_PX_RADIUS } from '@/config/constantes';
 // Styles
-// import styles from './StandardCard.module.css';
+import styles from './CustomCard.module.css';
 
 type CustomCardProps = {
 	children: React.ReactNode;
 };
 
 export const CustomCard = (props: CustomCardProps) => {
+	const { isDarkMode } = setDarkMode();
 	return (
-		<Card style={{ borderRadius: NATIVE_COMPONENT_PX_RADIUS }}>
+		<div
+			className={`${isDarkMode ? 'dark-theme' : ''} ${styles.CustomCard}`}
+			style={{ borderRadius: NATIVE_COMPONENT_PX_RADIUS }}
+		>
 			{props.children}
-		</Card>
+		</div>
 	);
 };
